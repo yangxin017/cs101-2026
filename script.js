8570db99b54ed534a87c354e8c60082a166304e1
yangxin017 <yangzin8002@gmail.com>
2026-04-08 14:48:19 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+var count=1;
+function addfunction(){
+    var btn=document.createElament("BUTTON");
+    btn.innerHTML=`CLICK ME(${count})`;
+    btn.setAttribute("id","btn_"+count++);
+    btn.setAttribute("class","btn btn-outline-danger");
+    console.log(btn);
+    document.body.appendChild(btn);
+};
+
+function delfunction(){
+    var btn=document.getElementById("btn_"+ --count);
+    console.log(btn);
+    document.body.removeChild(btn);
+}
